6735c3a683f159b14ee17dbb856768ccc7998155
IbaiBuR <64355006+IbaiBuR@users.noreply.github.com>
2023-07-07 17:18:58 +0200

    Improved, added README and compilation for Windows

---
+++ README.md
@@ -0,0 +1,44 @@
+# Experience Tracker
+
+Well, this is a simple project I wanted to overcome in order to decode and extract data from experience files, which are similar to a
+[persistent hash table](https://www.chessprogramming.org/Persistent_Hash_Table).
+
+# What is the format of the experience files?
+
+For both [BrainLearn](https://github.com/amchess/BrainLearn), [ShashChess](https://github.com/amchess/ShashChess) and Eman the format is the following:
+
+- 8 bytes to store the position's key, which is a [zobrist hash](https://www.chessprogramming.org/Zobrist_Hashing)
+- 4 bytes to store the rest
+  - Score, Depth, Move and Performance in the case of BrainLearn and ShashChess
+  - Score, Depth, Move and Count in the case of Eman
+
+So, each experience entry has a size of 24 bytes (192 bits)
+
+# How to use it?
+
+Its basic use is:
+
+- ./experience_tracker experience.bin (for Linux)
+- experience_tracker.exe experience.bin (for Windows)
+
+# What does it do
+
+For now, it converts part of the experience data to readable format and it removes all the depth 0 entries in the experience file
+
+## Example
+
+This is some sample output for the tool
+
+```
+Key: 537202e6, Move: a5b5, Score: -1, Depth: 54, Performance: 100
+Key: be03c7e3, Move: c8c5, Score: -47, Depth: 25, Performance: 100
+Key: 5b93aa11, Move: f6g5, Score: -8, Depth: 34, Performance: 100
+Key: f53d4941, Move: e6e5, Score: -15, Depth: 31, Performance: 100
+Key: 57de86fd, Move: f2c5, Score: 162, Depth: 32, Performance: 100
+```
+
+# To-Do
+
+- [x] Defrag the experience file
+- [ ] Defrag with specific minimum depth
+- [ ] Show experience entries for most common openings
--- main.c
+++ main.c
@@ -1,35 +1,53 @@
 #include <stdio.h>
 #include <string.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include "types.h"
 
-int main()
+int main(int argc, char *argv[])
 {
     FILE *BL_exp, *exp_fixed, *output_fixed, *BrainLearn_readable;
+    const char *dirname = "readable_exp";
+    DIR *readable_exp = opendir(dirname);
 
+    if(readable_exp)
+    {
+        closedir(readable_exp); //directory already exists
+    }
+    else
+    {
+        mkdir("./readable_exp", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH); //directory does not exist so we create it...
+    }
 
+    if(argc < 2)
+    {
+        printf("Incorrect number of parameters...\n");
+        return 1;
+    }
     
-    if(!(BL_exp = fopen("./experience_files/experience.bin", "rb")))
+    if(!(BL_exp = fopen(argv[1], "rb")))
     {
         printf("Could not open the BrainLearn experience file...\n");
-        return 1;
+        return 2;
     }
 
     if(!(BrainLearn_readable = fopen("./readable_exp/brainlearn.txt", "w")))
     {
         printf("Could not open brainlearn txt file to write...\n");
-        return 2;
+        return 3;
     }
 
-    if(!(exp_fixed = fopen("./experience_files/experience_fixed.bin", "wb")))
+    if(!(exp_fixed = fopen("experience_fixed.bin", "wb")))
     {
         printf("Could not open experience file to fix...\n");
-        return 3;
+        return 4;
     }
 
     if(!(output_fixed = fopen("./readable_exp/brainlearn_fixed.txt", "w")))
     {
         printf("Could not open brainlearn fixed txt file to write...\n");
-        return 4;
+        return 5;
     }
 
     write_BLexp_entry_toTxTFile(BL_exp, BrainLearn_readable);
@@ -37,10 +55,10 @@ int main()
 
     fclose(exp_fixed);
 
-    if(!(exp_fixed = fopen("./experience_files/experience_fixed.bin", "rb")))
+    if(!(exp_fixed = fopen("experience_fixed.bin", "rb")))
     {
       printf("Could not open fixed exp file to read...\n");
-      return 5;
+      return 6;
     }
     
     write_BLexp_entry_toTxTFile(exp_fixed, output_fixed);
